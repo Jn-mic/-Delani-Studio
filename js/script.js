894e7acedceb30d3a2a1c28da92e3bb3b51b1598
jn-mic <jackotienokey@gmail.com>
2021-04-19 00:03:59 +0300

    adding js logic

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-$(document).ready(function( ){
-    $('#imglogo').show();
-    $("#imgmouse").hide();
-    $('#imgmouse','#imglogo').onclick(function(){
-        $('#imgmouse','#imglogo').show();
-        $('this').hide();
-    });
+// $(document).ready(function( ){
+//     // $('#imgmouse','#imglogo').onclick(function(){
+//     //     $('#imgmouse','#imglogo').show();
+//     //     $('this').hide();
+//     // });
 
-    $('p.para1').css( 'color','green');
+//     // $('p.para1').css( 'color','green');
 
+// });
+$(document).ready(function(){
+    $('portfolio').onclick(function(){
+        $("row.col1").onclick(function(){
+            $("#pot1","pot2","#pot3","#pot4").show();
+            $("this").hide();
+        });
+    })
 });
