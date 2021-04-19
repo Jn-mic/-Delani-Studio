07b8d8a5b078e6aa4dcc3d62be54bd8fa2469caa
jn-mic <jackotienokey@gmail.com>
2021-04-19 10:55:44 +0300

    portfolio onclick

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-// $(document).ready(function( ){
-//     // $('#imgmouse','#imglogo').onclick(function(){
-//     //     $('#imgmouse','#imglogo').show();
-//     //     $('this').hide();
-//     // });
 
-//     // $('p.para1').css( 'color','green');
-
+// $(document).ready(function(){
+//     $('portfolio').onclick(function(){
+//         $("row.col1").onclick(function(){
+//             $("#pot1","pot2","#pot3","#pot4").show();
+//             $("this").hide();
+//         });
+//     })
 // });
+
 $(document).ready(function(){
-    $('portfolio').onclick(function(){
-        $("row.col1").onclick(function(){
-            $("#pot1","pot2","#pot3","#pot4").show();
-            $("this").hide();
-        });
-    })
+    $("#design").click(function(){
+        $("div.design",).toggle();
+    });
 });
 
 $(document).ready(function(){
+    $("#devp").click(function(){
+        $("div.devp").toggle();
+    });
+});
 
+$(document).ready(function(){
+    $("#prd").click(function(){
+        $("div.prd").toggle();
+    });
+});
+    
+$("#pot1").click(function(){
+    $("div.blackstudio").hover();
 });
